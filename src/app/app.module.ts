b8ce1c3d5f1e124e1724146baca088d249bc9a53
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-17 21:43:32 -0300

    Main routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { BreadcrumbComponent } from "./shared/breadcrumb/breadcrumb.component";
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { ProgressComponent } from "./pages/progress/progress.component";
+import { Graph1Component } from "./pages/graph1/graph1.component";
 
 @NgModule({
   declarations: [
@@ -19,12 +23,12 @@ import { HeaderComponent } from './shared/header/header.component';
     DashboardComponent,
     BreadcrumbComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProgressComponent,
+    Graph1Component,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
