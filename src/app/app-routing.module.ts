81dde80f7d589fea133fddd429afb8fadb2e40b7
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-18 14:01:05 -0300

    Secondary routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,35 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graph1Component } from './pages/graph1/graph1.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { ProgressComponent } from "./pages/progress/progress.component";
+import { Graph1Component } from "./pages/graph1/graph1.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
+import { PagesComponent } from "./pages/pages.component";
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'graph1', component: Graph1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-  { path: '**', component: NotFoundComponent }
+  {
+    path: "",
+    component: PagesComponent,
+    children: [
+      { path: "dashboard", component: DashboardComponent },
+      { path: "progress", component: ProgressComponent },
+      { path: "graph1", component: Graph1Component },
+      { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+    ],
+  },
+
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+
+  { path: "**", component: NotFoundComponent },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
