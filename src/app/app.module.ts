8533eb956e50aca956984b61016a8223737fc2df
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-19 17:37:36 -0300

    Add new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
+import { PagesModule } from "./pages/pages.module";
 
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./auth/login/login.component";
 import { RegisterComponent } from "./auth/register/register.component";
 import { NotFoundComponent } from "./pages/not-found/not-found.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { BreadcrumbComponent } from "./shared/breadcrumb/breadcrumb.component";
-import { SidebarComponent } from "./shared/sidebar/sidebar.component";
-import { HeaderComponent } from "./shared/header/header.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
-import { Graph1Component } from "./pages/graph1/graph1.component";
-import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -21,15 +15,8 @@ import { PagesComponent } from './pages/pages.component';
     LoginComponent,
     RegisterComponent,
     NotFoundComponent,
-    DashboardComponent,
-    BreadcrumbComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Graph1Component,
-    PagesComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, PagesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+
+import { SharedModule } from "../shared/shared.module";
+
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ProgressComponent } from "./progress/progress.component";
+import { Graph1Component } from "./graph1/graph1.component";
+import { PagesComponent } from "./pages.component";
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Graph1Component,
+    PagesComponent,
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graph1Component,
+    PagesComponent,
+  ],
+  imports: [CommonModule, RouterModule, SharedModule],
+})
+export class PagesModule {}
