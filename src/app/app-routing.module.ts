89bb261976613f444d027fb6a8ffccc92837c7d2
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-19 17:59:13 -0300

    Add AuthRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,18 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { PagesRoutingModule } from "./pages/pages.routing";
+import { AuthRoutingModule } from "./auth/auth.routing";
 
-import { RegisterComponent } from "./auth/register/register.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
-import { LoginComponent } from "./auth/login/login.component";
 
-const routes: Routes = [
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
-
-  { path: "**", component: NotFoundComponent },
-];
+const routes: Routes = [{ path: "**", component: NotFoundComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), PagesRoutingModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
