30db643075df6834bc712c6ec5595a5a3b116baf
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-19 17:54:15 -0300

    Add child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
+import { PagesRoutingModule } from "./pages/pages.routing";
+
 import { RegisterComponent } from "./auth/register/register.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
-import { Graph1Component } from "./pages/graph1/graph1.component";
 import { LoginComponent } from "./auth/login/login.component";
-import { PagesComponent } from "./pages/pages.component";
 
 const routes: Routes = [
-  {
-    path: "",
-    component: PagesComponent,
-    children: [
-      { path: "dashboard", component: DashboardComponent },
-      { path: "progress", component: ProgressComponent },
-      { path: "graph1", component: Graph1Component },
-      { path: "", redirectTo: "/dashboard", pathMatch: "full" },
-    ],
-  },
-
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
 
@@ -28,7 +15,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), PagesRoutingModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
