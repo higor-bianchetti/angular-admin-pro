909f068646343c47f263087772a42f1f6bf675fc
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-26 16:49:15 -0300

    Selecting a theme dynamically

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -7,15 +7,35 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AccountSettingsComponent implements OnInit {
   public linkTheme = document.querySelector("#theme");
+  public selectors: NodeListOf<Element>;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.selectors = document.querySelectorAll(".selector");
+    this.checkCurrentTheme();
+  }
 
   changeTheme(theme: string) {
     const url = `./assets/css/colors/${theme}.css`;
 
     this.linkTheme.setAttribute("href", url);
     localStorage.setItem("theme", theme);
+
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    this.selectors.forEach((element) => {
+      element.classList.remove("working");
+
+      const themeSelected = element.getAttribute("data-theme");
+      const urlThemeSelected = `./assets/css/colors/${themeSelected}.css`;
+      const currentTheme = this.linkTheme.getAttribute("href");
+
+      if (urlThemeSelected === currentTheme) {
+        element.classList.add("working");
+      }
+    });
   }
 }
