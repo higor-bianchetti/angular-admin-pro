3ce875cd06fee86d2479f54d4791df8156040591
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-26 16:33:39 -0300

    Add dynamic styles to the app

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-account-settings',
-  templateUrl: './account-settings.component.html',
-  styles: []
+  selector: "app-account-settings",
+  templateUrl: "./account-settings.component.html",
+  styles: [],
 })
 export class AccountSettingsComponent implements OnInit {
+  public linkTheme = document.querySelector("#theme");
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  changeTheme(theme: string) {
+    const url = `./assets/css/colors/${theme}.css`;
+
+    this.linkTheme.setAttribute("href", url);
+    localStorage.setItem("theme", theme);
   }
-
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pages',
-  templateUrl: './pages.component.html',
-  styles: []
+  selector: "app-pages",
+  templateUrl: "./pages.component.html",
+  styles: [],
 })
 export class PagesComponent implements OnInit {
+  public linkTheme = document.querySelector("#theme");
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const theme = localStorage.getItem("theme") || "default-dark";
 
+    const url = `./assets/css/colors/${theme}.css`;
+    this.linkTheme.setAttribute("href", url);
+  }
 }
