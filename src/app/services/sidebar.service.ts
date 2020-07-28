12c44cd0df9326d9734b9a478dc9f2ed9e02f6dc
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-28 19:41:51 -0300

    Add SidebarService

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class SidebarService {
+  menu: any[] = [
+    {
+      title: "Dashboard",
+      icon: "mdi mdi-gauge",
+      submenu: [
+        { title: "Home", url: "/" },
+        { title: "Progress", url: "progress" },
+        { title: "Graphs", url: "graph1" },
+      ],
+    },
+  ];
+
+  constructor() {}
+}
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { SidebarService } from "../../services/sidebar.service";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styles: []
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styles: [],
 })
 export class SidebarComponent implements OnInit {
+  menuItems: any[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private sidebarService: SidebarService) {
+    this.menuItems = sidebarService.menu;
   }
 
+  ngOnInit() {}
 }
