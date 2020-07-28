cf82caad1dfa603a7926fee10cc03be6edabb76f
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-28 19:12:34 -0300

    Add router to account-settings

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
 
 import { BreadcrumbComponent } from "./breadcrumb/breadcrumb.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
@@ -8,6 +9,6 @@ import { HeaderComponent } from "./header/header.component";
 @NgModule({
   declarations: [BreadcrumbComponent, SidebarComponent, HeaderComponent],
   exports: [BreadcrumbComponent, SidebarComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
