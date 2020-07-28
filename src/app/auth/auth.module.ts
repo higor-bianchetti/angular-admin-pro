16b092050b4fc431260574ecd9380b96a1d4ce47
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-28 19:42:26 -0300

    Calling a global function to initialize scripts

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   exports: [LoginComponent, RegisterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RouterModule],
 })
 export class AuthModule {}
