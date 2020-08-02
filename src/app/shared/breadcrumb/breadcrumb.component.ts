c78bde5aa6c332e66d4ba1ae34839acab5e7e460
Higor Bianchetti <higorbianchetti@gmail.com>
2020-08-02 02:07:15 -0300

    Optimizing breadcrumb

--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,29 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy } from "@angular/core";
 import { Router, ActivationEnd } from "@angular/router";
 import { filter, map } from "rxjs/operators";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-breadcrumb",
   templateUrl: "./breadcrumb.component.html",
   styles: [],
 })
-export class BreadcrumbComponent {
+export class BreadcrumbComponent implements OnDestroy {
   public title: string;
+  public titleSubscription: Subscription;
 
   constructor(private router: Router) {
-    this.getDataRouting();
+    this.titleSubscription = this.getDataRouting().subscribe((data) => {
+      this.title = data.title;
+      document.title = `Admin Pro - ${data.title}`;
+    });
   }
 
   getDataRouting() {
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof ActivationEnd),
-        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
-        map((event: ActivationEnd) => event.snapshot.data)
-      )
-      .subscribe((data) => {
-        this.title = data.title;
-        document.title = `Admin Pro - ${data.title}`;
-      });
+    return this.router.events.pipe(
+      filter((event) => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.titleSubscription.unsubscribe();
   }
 }
