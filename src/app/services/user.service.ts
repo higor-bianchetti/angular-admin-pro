4a1a11f1a7cc36a75037d14bbd11a4234d2b4fd8
Higor Bianchetti <higorbianchetti@gmail.com>
2020-09-11 20:35:59 -0300

    Save token on localStorage

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { tap } from "rxjs/operators";
 
 import { RegisterForm } from "../interfaces/register-form.interface";
 import { LoginForm } from "../interfaces/login-form.interface";
@@ -14,10 +15,20 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   createUser(formData: RegisterForm) {
-    return this.http.post(`${base_url}/users`, formData);
+    return this.http.post(`${base_url}/users`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem("token", resp.token);
+        console.log(resp);
+      })
+    );
   }
 
   login(formData: LoginForm) {
-    return this.http.post(`${base_url}/login`, formData);
+    return this.http.post(`${base_url}/login`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem("token", resp.token);
+        console.log(resp);
+      })
+    );
   }
 }
