778ce5e964552b52fad4428ce300ed2b3f69075a
Higor Bianchetti <higorbianchetti@gmail.com>
2020-09-11 20:29:31 -0300

    Login with email

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
+import Swal from "sweetalert2";
+
+import { UserService } from "../../services/user.service";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"],
 })
-export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
+export class LoginComponent {
+  public formSubmitted = false;
 
-  ngOnInit() {}
+  public loginForm = this.formBuilder.group({
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", Validators.required],
+    remember: [false],
+  });
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) {}
 
   login() {
-    this.router.navigateByUrl("/");
+    // this.router.navigateByUrl("/");
+    this.userService.login(this.loginForm.value).subscribe(
+      (resp) => {
+        console.log(resp);
+      },
+      (err) => {
+        Swal.fire("Error", err.error.msg, "error");
+      }
+    );
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 import { RegisterForm } from "../interfaces/register-form.interface";
+import { LoginForm } from "../interfaces/login-form.interface";
 import { environment } from "../../environments/environment";
 
 const base_url = environment.base_url;
@@ -14,5 +15,9 @@ export class UserService {
 
   createUser(formData: RegisterForm) {
     return this.http.post(`${base_url}/users`, formData);
+  }
+
+  login(formData: LoginForm) {
+    return this.http.post(`${base_url}/login`, formData);
   }
 }
